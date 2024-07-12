952f3074608d3fe34875fe6a7dc01ad8fe6a7191
lars.van.driel <lars.van.driel@hotmail.com>
2024-07-13 01:36:15 +0200

    Changed language to English

--- TpvTool/Models/Category.cs
+++ TpvTool/Models/Category.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Models
 {
-    internal class Categorie
+    public class Category
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
     }
 }
--- TpvTool/Models/Product.cs
+++ TpvTool/Models/Product.cs
@@ -1,7 +1,10 @@
 ﻿namespace Models
 {
-    public class Class1
+    public class Product
     {
-
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
     }
 }
